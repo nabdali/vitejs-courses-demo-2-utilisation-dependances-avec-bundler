2e85cdc26374eb9334f058264d518df7fdd1821d
nabdali <224860@university-365.com>
2024-12-10 10:31:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-courses-demo-2-utilisation-dependances-avec-bundler
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nabdali/vitejs-courses-demo-2-utilisation-dependances-avec-bundler)
---
+++ src/main.js
@@ -0,0 +1,28 @@
+import './style.css';
+import javascriptLogo from './javascript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.js';
+import { arrayShuffle } from 'lodash/_arrayShuffle';
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`;
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arrayShuffle(arr)); // Affiche un tableau mélangé
+
+setupCounter(document.querySelector('#counter'));
